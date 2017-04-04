8978e0bc8d1649cabbecb13ad8a98586842f4412
Megan Flood <mak.flood@comcast.net>
2017-04-04 12:21:48 -0700

    added the html, css, and js files

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('lulwat this rocks');
+
+var sushi = prompt('do you think i like sushi?');
+
+if (sushi.toLowerCase().trim() == 'yes') {
+  alert('you were correct');
+} else {
+  alert('you are clueless about me');
+}
