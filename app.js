68ead078ce663a92eccffb385030e554bbd3b1bc
Megan Flood <mak.flood@comcast.net>
2017-04-05 15:31:51 -0700

    added a question that gives the user 4 chances to guess a random number from 0-100

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 'use strict';
 
-// yes/no question, correct answer, response to yes, response to no
-var ynQuestions = [['Am I alive?', 'YES', 'Affirmative, I am a living creature.', 'Negative, I am not a computer...'],
-                   ['Am I a mammal?', 'YES', 'I am indeed a mammal.', 'Not an insect...'],
-                   ['Am I smaller than a breadbox?', 'NO', 'Quite a bit larger in fact.', 'Haha, no.'],
-                   ['Am I female?', 'YES', 'Yes, of the rare minority.', 'Nope. Nope. Nope.'],
-                   ['Am I Megan?', 'YES', 'Yep, that\'s my name!', 'Seriously?']];
-
 var correctCount = 0; // counter for correct answers
 
 var user = prompt('Hey, what\'s your name?');
@@ -20,11 +13,21 @@ if (hasName) {
 }
 alert('Hello, ' + printUser + '. I\'m going to ask some questions about me, try and get them all right!');
 
+//YES AND NO QUESTIONS
+
+// yes/no question, correct answer, response to yes, response to no
+var ynQuestions = [['Am I alive?', 'YES', 'Affirmative, I am a living creature.', 'Negative, I am not a computer...'],
+                   ['Am I a mammal?', 'YES', 'I am indeed a mammal.', 'Not an insect...'],
+                   ['Am I smaller than a breadbox?', 'NO', 'Quite a bit larger in fact.', 'Haha, no.'],
+                   ['Am I female?', 'YES', 'Yes, of the rare minority.', 'Nope. Nope. Nope.'],
+                   ['Am I Megan?', 'YES', 'Yep, that\'s my name!', 'Seriously?']];
+
 var userAns;
 var question;
 var correctAns;
 var correctResp;
 var wrongResp;
+
 for (var ask = 0; ask < ynQuestions.length; ask++) {
   question = ynQuestions[ask][0];
   correctAns = ynQuestions[ask][1];
@@ -36,7 +39,7 @@ for (var ask = 0; ask < ynQuestions.length; ask++) {
       userAns = userAns.toUpperCase().trim();
       userAns = userAns.charAt(0) === correctAns.charAt(0);
     }
-    console.log(question, userAns);
+    console.log(question, 'user correct:', userAns);
   } while (typeof userAns != 'boolean')
   if (userAns) {
     alert(correctResp);
@@ -45,38 +48,53 @@ for (var ask = 0; ask < ynQuestions.length; ask++) {
     alert(wrongResp);
   }
 }
-console.log(correctCount);
+console.log('correct y/n answers:', correctCount);
 
-// /**
-// Takes the return from a prompt() and returns if yes or y were entered. Returns null if given a null.
-// **/
-// var checkInput = function(input) {
-//  if (typeof input != 'string') {
-//    return input;
-//  } else {
-//    var tInput = input.toUpperCase().trim();
-//    return tInput == 'YES' || tInput == 'Y';
-//  }
-// };
-//
-// /**
-// Takes the question to be asked in a prompt, the answer for a response of 'yes', and the answer for a response of 'no'. Asks the question until the 'OK' button is pressed. Prints the user response as a boolean or null to the console. Responds to the user with the given answer in an alert. Returns nothing.
-// **/
-// var askQuestion = function(question, yesAns, noAns) {
-//   var response;
-//   do {
-//     response = checkInput(prompt(question));
-//     console.log(question, response);
-//   } while (typeof response != 'boolean')
-//   if (response) {
-//     alert(yesAns);
-//   } else {
-//     alert(noAns);
-//   }
-// };
-//
-// askQuestion('Am I alive?', 'Affirmative, I am a living creature.', 'Negative, I am not a computer...');
-// askQuestion('Am I a mammal?', 'I am indeed a mammal.', 'Not an insect...');
-// askQuestion('Am I smaller than a breadbox?', 'Haha, no.', 'Quite a bit larger in fact.');
-// askQuestion('Am I female?', 'Yes, of the rare minority.', 'Nope. Nope. Nope.');
-// askQuestion('Am I Megan?', 'Yay, you win! :D', 'Seriously?');
+// GUESS THE NUMBER QUESTIONS
+
+// userAns defined above
+
+var favNum = Math.round((Math.random() * 100));
+console.log('my favorite number is:', favNum);
+
+var isNotDone = true;
+var message;
+
+for (var guess = 3; guess >= 0 && isNotDone; guess--) {
+
+  userAns = prompt('Guess my favorite number!');
+  console.log('user gueses:',userAns);
+  if (isNaN(userAns)) {
+    alert('That\'s not a number...')
+  } else {
+    userAns = parseInt(userAns);
+    isNotDone = userAns != favNum;
+
+    if (isNotDone) {
+      message = 'No, that number is too ';
+      if (userAns > favNum) {
+        message += 'large.';
+      } else {
+        message += 'small.';
+      }
+      if (guess != 0) {
+        message += '\nOnly ' + guess + ' ';
+        if (guess != 1) {
+          message += 'guesses remain.';
+        } else {
+          message += 'guess remains.';
+        }
+      } else {
+        message += '\nToo bad, all out of guesses.\nIt was ' + favNum + '.';
+      }
+      alert(message);
+      
+    } else {
+      alert('Yay! You guessed right!');
+      correctCount++;
+    }
+
+  }
+}
+
+console.log('current correct answers:', correctCount);
